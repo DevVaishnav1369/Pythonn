391ecb4f8bdb734e65cd5f211347e567b2becd55
DevVaishnav1369 <devvaishnav2007@gmail.com>
2026-02-25 17:11:24 +0530

    practice python

--- tech_radar/main.py
+++ tech_radar/main.py
@@ -1,2 +1,10 @@
 import math, random
-print(random.randint(1, 1000))
+n = int (input("Enter number :" ))
+if (n == 0 or n == 1 ):
+    print ("Not a prime number")
+else:
+    for i in range (2,n+1):
+        if (n % i == 0):
+            print ( i )
+            break
+    else: print ("Prime number")
